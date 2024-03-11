ea9b2671107888dffc27a3860f7a10ee428aff69
Yooinhak <inhak.yoo@corretto.io>
2024-03-11 09:22:23 +0900

    feat: env setting

---
+++ next.config.js
@@ -0,0 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // pageExtensions: ['page.jsx', 'api.js'],
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+      pure: true,
+    },
+  },
+  serverRuntimeConfig: {
+    mode: process.env.MODE,
+  },
+  publicRuntimeConfig: {
+    appVersion: process.env.APP_VERSION,
+  },
+};
+
+module.exports = nextConfig;
