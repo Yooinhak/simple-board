ae31de595ed08bfbeec49caeaa4a3ab3d7dc1259
Yooinhak <syu3236@gmail.com>
2024-03-14 18:09:42 +0900

    feat: styled table

---
+++ src/components/Table/component.tsx
@@ -0,0 +1,292 @@
+import styled from "styled-components";
+
+const Component = () => {
+  return (
+    <Wrapper>
+      <TableHeader>
+        <Table cellPadding={0} cellSpacing={0} border={0}>
+          <thead>
+            <tr>
+              <th>Code</th>
+              <th>Company</th>
+              <th>Price</th>
+              <th>Change</th>
+              <th>Change %</th>
+            </tr>
+          </thead>
+        </Table>
+      </TableHeader>
+      <TableContent>
+        <Table cellPadding={0} cellSpacing={0} border={0}>
+          <tbody>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+            <tr>
+              <td>AAC</td>
+              <td>AUSTRALIAN COMPANY </td>
+              <td>$1.38</td>
+              <td>+2.01</td>
+              <td>-0.36%</td>
+            </tr>
+            <tr>
+              <td>AAD</td>
+              <td>AUSENCO</td>
+              <td>$2.38</td>
+              <td>-0.01</td>
+              <td>-1.36%</td>
+            </tr>
+            <tr>
+              <td>AAX</td>
+              <td>ADELAIDE</td>
+              <td>$3.22</td>
+              <td>+0.01</td>
+              <td>+1.36%</td>
+            </tr>
+            <tr>
+              <td>XXD</td>
+              <td>ADITYA BIRLA</td>
+              <td>$1.02</td>
+              <td>-1.01</td>
+              <td>+2.36%</td>
+            </tr>
+          </tbody>
+        </Table>
+      </TableContent>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+const Table = styled.table`
+  width: 100%;
+  table-layout: fixed;
+
+  th {
+    padding: 20px 15px;
+    text-align: left;
+    font-weight: 500;
+    font-size: 12px;
+    color: #fff;
+    text-transform: uppercase;
+  }
+  td {
+    padding: 15px;
+    text-align: left;
+    vertical-align: middle;
+    font-weight: 300;
+    font-size: 12px;
+    color: #fff;
+    border-bottom: solid 1px rgba(255, 255, 255, 0.1);
+  }
+`;
+const TableHeader = styled.div`
+  background-color: rgba(255, 255, 255, 0.3);
+`;
+const TableContent = styled.div`
+  height: 300px;
+  overflow-x: auto;
+  margin-top: 0px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+`;
+
+export default Component;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,8 @@
 import getConfig from "next/config";
 import Head from "next/head";
 import { useState } from "react";
+import styled, { keyframes } from "styled-components";
+import Table from "@components/Table";
 
 export default function Home() {
   const [title, setTitle] = useState("");
@@ -46,12 +48,38 @@ export default function Home() {
       <Head>
         <title>Simple_board</title>
       </Head>
-      <main>
-        <button onClick={fetchPosts}>test</button>
+      <Wrapper>
+        {/* <button onClick={fetchPosts}>test</button>
         <button onClick={createPost}>만들기</button>
         <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        <input value={contents} onChange={(e) => setContents(e.target.value)} />
-      </main>
+        <input value={contents} onChange={(e) => setContents(e.target.value)} /> */}
+        <Table />
+      </Wrapper>
     </>
   );
 }
+
+const Gradient = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(132deg, #fc415a, #591bc5, #212335);
+  background-size: 400% 400%;
+  animation: ${Gradient} 15s ease infinite;
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  padding: 0;
+  margin: 0px;
+  padding: 32px;
+`;
