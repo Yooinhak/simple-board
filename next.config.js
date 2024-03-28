72eda55f6226d45b4ebd27dacf603a6a1138cc37
Yooinhak <syu3236@gmail.com>
2024-03-28 16:42:12 +0900

    fix: next config export remove

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
-  // pageExtensions: ['page.jsx', 'api.js'],
   compiler: {
     styledComponents: {
       displayName: true,
